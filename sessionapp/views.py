c2343470e9a450f8e2bec39c0e6cfeb004e63a30
Sreehari738 <naidusreehari@68gmail.com>
2021-12-30 16:35:30 +0530

    new session

---
+++ sessionapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def Welcome(request):
+    return render(request,'show.html')
+def Setattributes(request):
+
+    request.session["value1"] = request.POST["t1"]
+    request.session["value2"] = request.POST["t2"]
+    request.session.set_expiry(30)
+    return render(request,'getvalue.html')
+def Getattributes(request):
+    val3= request.session["value1"]
+
+    val4 = request.session["value2"]
+    return render(request,'showvalue.html',locals())
+
+
